64d52494699c5803183c8b6ce16e9e0b00abcdb3
Lahiru Sandaruwan Jayawardana <147098751+Sandaruwan2000@users.noreply.github.com>
2024-08-31 11:07:34 +0530

    finish

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,6 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import authRoutes from './routes/auth.route.js';
-import userRoutes from './routes/user.route.js';
 import cartRoutes from './routes/cart.route.js';
-import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
 import cors from 'cors';
 
@@ -23,7 +20,7 @@ const app = express();
 
 app.use(express.json());
 
-app.use(cookieParser()) ;
+
 
 app.use(cors({
   origin: 'http://localhost:8081',
@@ -34,8 +31,7 @@ app.listen(3000, () => {
   }
   );
 
-  app.use('/backend/auth', authRoutes);
-  app.use('/backend/user', userRoutes);
+ 
   app.use('/backend/cart', cartRoutes);
  
 
